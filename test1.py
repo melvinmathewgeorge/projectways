3d19eeea93e095fdc36e1cdafb24f739503dbbeb
melvinmathewgeorge <melvingeorge29@gmail.com>
2024-02-20 12:37:24 +0530

    first commit

---
+++ test1.py
@@ -0,0 +1,111 @@
+#reverse
+
+a=num[::-1]
+print(a)
+
+for m in range(3):
+    print(m)
+n=[9,8,7,6,5,4,3,2,1]  
+
+str1='Count'
+
+print(len(str1))
+ 
+
+'''
+print(num[2])
+print(num[0::2])
+print(num[:])
+print(num[-6:-3])
+print(num[-6:-1])
+print(num[-4])
+
+num.sort()
+print(num)
+
+num.reverse()
+print(num)
+1. Make it list to string
+2.
+info="My Name is Melvin"
+output=["My", "Name", "is", "Melvin"]
+ou="Melvin is Name My"
+'''
+info2="I Want To Be a Developer"
+info3=info2.split()
+print(info3)
+info4=info3[::-1]
+print(info4)
+info5="---".join(info4)
+print(info5)
+
+print(range(1, 11, 1))
+
+l = [1,2,3,4,5,100,200,14]
+for j in range(len(l)):
+    print(l[5])
+for i in range(len(info5)):
+    print(info5[i])
+
+
+
+print(range(10))
+
+
+str_lis1 = "I want to be a developer. Python is my favorite programming language"
+str_lis=str_lis1.split()
+print(type(str_lis))
+print(str_lis)
+
+for s in range(len(str_lis)):
+    
+     print(f"The length of {str_lis[s]} is ",len(str_lis[s]))
+
+
+
+lst9=[1,20,33,4,45,6,7,80,90,100]
+
+for m in range(len(lst9)):
+    if lst9[m]%2==0:
+     print(lst9[m], 'is a even number')
+    else:
+     print(lst9[m], 'is a odd number')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+info1="My Name is Melvin"
+lis_info = info1.split()
+print(lis_info)
+lis_info2=lis_info[::-1]
+lstr = " ".join(lis_info2)
+print(lstr)
+
+'''
+info=["My","Name","is","Melvin"]
+
+for i in info:
+ j=i[::-1]
+ print(j)
+   '''  
+ 
+    
+
+
+
+'''
+
+print(num[::-1])
+print(num[5:6])
+print(num[-3:])
+'''
